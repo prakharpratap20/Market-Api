e5685cd344cf0a9a39a8d49e3b1b570d1d9fbdc9
Prakhar Pratap <prakharpratap2001@gmail.com>
2024-04-29 17:46:41 +0530

    working with flask smorest

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import uuid
 from flask import Flask, request
+from flask_smorest import abort
 from db import items, stores
 
 app = Flask(__name__)
@@ -13,6 +14,14 @@ def get_stores():
 @app.post("/store")
 def create_store():
     store_data = request.get_json()
+
+    if "name" not in store_data:
+        abort(400, message="Bad request. Ensure 'name' is included in the JSON payload.")
+
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, message=f"Store already exists.")
+
     store_id = uuid.uuid4().hex
     store = {**store_data, "id": store_id}
     stores[store_id] = store
@@ -22,8 +31,16 @@ def create_store():
 @app.post("/item")
 def create_item(name):
     item_data = request.get_json()
+
+    if ("price" not in item_data or "store_id" not in item_data or "name" not in item_data):
+        abort(400, message="Bad request. Ensure 'price', 'store_id' and 'name' are included in the JSON payload.")
+
+    for item in item.values():
+        if (item_data["name"] == item["name"] and item_data["store_id"] == item["store_id"]):
+            abort(400, message=f"Item already exists.")
+
     if item_data["store_id"] not in stores:
-        return {"message": "Store not found"}, 404
+        abort(404, message="Store not found.")
 
     item_id = uuid.uuid4().hex
     item = {**item_data, "id": item_id}
@@ -42,7 +59,7 @@ def get_store(store_id):
     try:
         return stores[store_id]
     except KeyError:
-        return {"message": "Store not found"}, 404
+        abort(404, message="Store not found.")
 
 
 @app.get("/item/<string:item_id>")
@@ -50,4 +67,4 @@ def get_item(item_id):
     try:
         return items[item_id]
     except KeyError:
-        return {"message": "Item not found"}, 404
+        abort(404, message="Item not found.")
