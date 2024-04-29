9da6073c6dc32fff4d149d176932459fe37c612e
Prakhar Pratap <prakharpratap2001@gmail.com>
2024-04-29 18:53:40 +0530

    update and delete

--- app.py
+++ app.py
@@ -29,13 +29,13 @@ def create_store():
 
 
 @app.post("/item")
-def create_item(name):
+def create_item():
     item_data = request.get_json()
 
     if ("price" not in item_data or "store_id" not in item_data or "name" not in item_data):
         abort(400, message="Bad request. Ensure 'price', 'store_id' and 'name' are included in the JSON payload.")
 
-    for item in item.values():
+    for item in items.values():
         if (item_data["name"] == item["name"] and item_data["store_id"] == item["store_id"]):
             abort(400, message=f"Item already exists.")
 
@@ -47,11 +47,6 @@ def create_item(name):
     items[item_id] = item
 
     return item, 201
-
-
-@app.get("/item")
-def get_all_items():
-    return {"items": list(items.values())}
 
 
 @app.get("/store/<string:store_id>")
@@ -68,3 +63,32 @@ def get_item(item_id):
         return items[item_id]
     except KeyError:
         abort(404, message="Item not found.")
+
+
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message": "Item deleted."}
+    except KeyError:
+        abort(404, message="Item not found.")
+
+
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    if ("price" not in item_data or "name" not in item_data):
+        abort(400, message="Bad request. Ensure 'price' and 'name' are included in the JSON payload.")
+
+    try:
+        item = items[item_id]
+        item |= item_data
+
+        return item
+    except KeyError:
+        abort(404, message="Item not found.")
+
+
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
--- db.py
+++ db.py
@@ -1,11 +1,2 @@
 stores = {}
-items = {
-    1: {
-        "name": "Chair",
-        "price": 17.99
-    },
-    2: {
-        "name": "Table",
-        "price": 79.99
-    }
-}
+items = {}
